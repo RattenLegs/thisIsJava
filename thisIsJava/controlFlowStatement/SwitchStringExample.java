cf08f83914b11451c3163ff8bdd18763bea742b5
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-08 05:41:17 +0900

    Add files via upload

---
+++ thisIsJava/controlFlowStatement/SwitchStringExample.java
@@ -0,0 +1,21 @@
+package controlFlowStatement;
+
+public class SwitchStringExample {
+
+	public static void main(String[] args) {
+		String position = "과장";
+
+		switch (position) {
+		case "부장":
+			System.out.println("700만원");
+			break;
+		case "과장":
+			System.out.println("500마누언");
+			break;
+		default:
+			System.out.println("300만원");
+		}
+
+	}
+
+}
