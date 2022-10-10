6827b6c1e4fc6c85100c048ae0352267c2c42d06
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-10 21:05:07 +0900

    Add files via upload

---
+++ thisIsJava/controlFlowStatement/ForSumFrom1To100Example.java
@@ -0,0 +1,15 @@
+package controlFlowStatement;
+
+public class ForSumFrom1To100Example {
+
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
---
+++ thisIsJava/controlFlowStatement/WhileKeycontrolExample.java
@@ -0,0 +1,10 @@
+package controlFlowStatement;
+
+public class WhileKeycontrolExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
