15f49d2ef6f7d4ed8d50004ac97acf201d0b0efe
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-11 21:14:17 +0900

    Add files via upload

---
+++ thisIsJava/thisIsJava/controlFlowStatement/IfNestedExample.java
@@ -0,0 +1,27 @@
+package controlFlowStatement;
+
+public class IfNestedExample {
+
+	public static void main(String[] args) {
+		int score = (int) (Math.random() * 20) + 81;
+
+		String grade;
+
+		if (score >= 90) {
+			if (score >= 95) {
+				grade = "A+";
+			} else {
+				grade = "A";
+			}
+		} else {
+			if (score >= 85) {
+				grade = "B+";
+			} else {
+				grade = "B";
+			}
+		}
+
+		System.out.println("ÇÐÁ¡: " + grade);
+	}
+
+}
