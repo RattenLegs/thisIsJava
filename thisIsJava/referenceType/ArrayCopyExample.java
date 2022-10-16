52b33dbec0b967100d9c8409e37f2ba8d3a91f5d
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-16 23:46:52 +0900

    Add files via upload

---
+++ thisIsJava/referenceType/ArrayCopyExample.java
@@ -0,0 +1,18 @@
+package referenceType;
+
+public class ArrayCopyExample {
+
+	public static void main(String[] args) {
+		String[] oldIntArray = { "java", "array", "copy" };
+		String[] newIntArray = new String[5];
+
+		for (int i = 0; i < oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+
+		for (int i = 0; i < newIntArray.length; i++) {
+			System.out.println(newIntArray[i] + ", ");
+		}
+	}
+
+}
