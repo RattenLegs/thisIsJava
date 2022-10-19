0ef80f609d23aa4f6105fcdf824a5aeca5ba4316
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-19 23:17:28 +0900

    Add files via upload

--- thisIsJava/thisisjavaclass/Car.java
+++ thisIsJava/thisisjavaclass/Car.java
@@ -1,10 +1,39 @@
 package thisisjavaclass;
 
 public class Car {
-	// 필드
+	/*
+	 * // 필드 String company = "현대자동차"; String model = "그랜저"; String color = "검정";
+	 * int maxSpeed = 350; int speed;
+	 */
+
+	/*
+	 * Car(String color, int cc) {
+	 * 
+	 * 
+	 * }
+	 */
+
 	String company = "현대자동차";
-	String model = "그랜저";
-	String color = "검정";
-	int maxSpeed = 350;
-	int speed;
+	String model;
+	String color;
+	int maxSpeed;
+
+	Car() {
+
+	}
+
+	Car(String model) {
+		this.model = model;
+	}
+
+	Car(String model, String color) {
+		this.model = model;
+		this.color = color;
+	}
+
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
 }
