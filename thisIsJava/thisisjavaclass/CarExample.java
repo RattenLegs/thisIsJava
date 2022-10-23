278844328937f2e562cf88a426de71ace518b9c8
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-23 23:51:18 +0900

    Add files via upload

--- thisIsJava/thisisjavaclass/CarExample.java
+++ thisIsJava/thisisjavaclass/CarExample.java
@@ -20,11 +20,13 @@ public class CarExample {
 
 		Car car1 = new Car();
 		System.out.println("car1.company: " + car1.company);
+		System.out.println("car1.model: " + car1.model);
 		System.out.println();
 
 		Car car2 = new Car("자가용");
 		System.out.println("car2.company: " + car2.company);
 		System.out.println("car2.model: " + car2.model);
+		System.out.println("car2.color: " + car2.color);
 		System.out.println();
 
 		Car car3 = new Car("자가용", "빨강");
