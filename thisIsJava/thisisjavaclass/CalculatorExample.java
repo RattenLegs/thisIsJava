a605029664460f81835ff428a1fb8df6ab9340dc
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-10-25 23:36:09 +0900

    Add files via upload

--- thisIsJava/thisisjavaclass/CalculatorExample.java
+++ thisIsJava/thisisjavaclass/CalculatorExample.java
@@ -4,18 +4,32 @@ public class CalculatorExample {
 
 	public static void main(String[] args) {
 		Calculator myCalc = new Calculator();
-		myCalc.powerOn();
+		/*
+		 * myCalc.powerOn();
+		 * 
+		 * int result1 = myCalc.plus(5, 6); System.out.println("result1: " + result1);
+		 * 
+		 * byte x = 10; byte y = 4;
+		 * 
+		 * double result2 = myCalc.divide(x, y); System.out.println("result2: " +
+		 * result2);
+		 * 
+		 * myCalc.powerOff();
+		 */
 
-		int result1 = myCalc.plus(5, 6);
-		System.out.println("result1: " + result1);
+		/*
+		 * // 클래스 내부에서 메소드 호출 myCalc.execute();
+		 */
 
-		byte x = 10;
-		byte y = 4;
+		// 정사각형의 넓이 구하기
+		double result1 = myCalc.areaRectangle(10);
 
-		double result2 = myCalc.divide(x, y);
-		System.out.println("result2: " + result2);
+		// 직사각형의 넓이 구하기
+		double result2 = myCalc.areaRectangle(10, 20);
 
-		myCalc.powerOff();
+		// 결과 출력
+		System.out.println("정사각형 넓이= " + result1);
+		System.out.println("직사각형 넓이= " + result2);
 	}
 
 }
--- thisIsJava/thisisjavaclass/Car.java
+++ thisIsJava/thisisjavaclass/Car.java
@@ -13,35 +13,65 @@ public class Car {
 	 * }
 	 */
 
-	String company = "현대자동차";
-	String model;
-	String color;
-	int maxSpeed;
-
-	Car() {
-
-	}
-
 	/*
+	 * String company = "현대자동차"; String model; String color; int maxSpeed;
+	 * 
+	 * Car() {
+	 * 
+	 * }
+	 * 
+	 * 
 	 * Car(String model) { this.model = model; }
 	 * 
 	 * Car(String model, String color) { this.model = model; this.color = color; }
 	 * 
 	 * Car(String model, String color, int maxSpeed) { this.model = model;
 	 * this.color = color; this.maxSpeed = maxSpeed; }
+	 * 
+	 * 
+	 * Car(String model) { this(model, "은색", 250); }
+	 * 
+	 * Car(String model, String color) { this(model, color, 250); }
+	 * 
+	 * Car(String model, String color, int maxSpeed) { this.model = model;
+	 * this.color = color; this.maxSpeed = maxSpeed; }
 	 */
 
-	Car(String model) {
-		this(model, "은색", 250);
+	/*
+	 * // 필드 int gas;
+	 * 
+	 * // 생성자
+	 * 
+	 * // 메소드 void setGas(int gas) { this.gas = gas; }
+	 * 
+	 * boolean isLeftGas() { if (gas == 0) { System.out.println("가스가 없습니다."); return
+	 * false; } else { System.out.println("gas가 " + gas + "만큼 있습니다."); return true;
+	 * } }
+	 * 
+	 * void run() { while (true) { if (gas > 0) { System.out.println("달립니다.(gas잔량:"
+	 * + gas + ")"); gas -= 1; } else { System.out.println("멈춥니다.(gas잔량:" + gas +
+	 * ")"); return; // 메소드 실행 종료 } } }
+	 */
+
+	// 클래스 외부에서 메소드 호출
+	// 필드
+	int speed;
+
+	// 생성자
+
+	// 메소드
+	int getSpeed() {
+		return speed;
 	}
 
-	Car(String model, String color) {
-		this(model, color, 250);
+	void keyTurnOn() {
+		System.out.println("키를 돌립니다.");
 	}
 
-	Car(String model, String color, int maxSpeed) {
-		this.model = model;
-		this.color = color;
-		this.maxSpeed = maxSpeed;
+	void run() {
+		for (int i = 10; i <= 50; i += 10) {
+			speed = i;
+			System.out.println("달립니다.(시속: " + speed + "km/h)");
+		}
 	}
 }
